07ef742531068fb9dc7b87e77c05946c01671b1c
Frederik Mazur Andersen <fm@fmazur.dev>
2021-09-01 21:29:38 +0200

    fixed scifi issue and added more details

--- routes/index.js
+++ routes/index.js
@@ -5,40 +5,45 @@ const query = require("source-server-query");
 const utf8 = require('utf8');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+    let serverInfo = await query.info("194.147.122.223", 2303, 500);
+
+    // console.log(serverInfo);
+    // console.log(serverInfo.maxplayers);
+    // console.log(serverInfo.playersnum);
 
     // get query
     query.rules("194.147.122.223", 2303, 500)
         .then(function (mods) {
-            var modpack = "Could not determine";
+                var modpack = "Could not determine";
 
-            // loop to see what modpack
-            for (const val of mods) {
-                // check if part of modern modpack
-                if (utf8.encode(val.value).includes("unsung")) {
-                    modpack = "Historical";
-                    break;
-                }
+                // loop to see what modpack
+                for (const val of mods) {
+                    // check if part of modern modpack
+                    if (utf8.encode(val.value).includes("unsung")) {
+                        modpack = "Historical";
+                        break;
+                    }
 
-                // check if part of historical modpack
-                if (utf8.encode(val.value).includes("RHSGREF")) {
-                    modpack = "Modern";
-                    break;
-                }
+                    // check if part of historical modpack
+                    if (utf8.encode(val.value).includes("RHSGREF")) {
+                        modpack = "Modern";
+                        break;
+                    }
 
-                // check if part of scifi modpack
-                if (utf8.encode(val.value).includes("Operation TREBUCHET")) {
-                    modpack = "Sci-fi";
-                    break;
+                    // check if part of scifi modpack
+                    if (utf8.encode(val.value).includes("Operation: TREBUCHET")) {
+                        modpack = "Sci-fi";
+                        break;
+                    }
                 }
+                // render result
+                res.render('index', {title: 'Digby Modpack Status', currentModpack: modpack, playernum: serverInfo.playersnum, maxPlayers: serverInfo.maxplayers, map: serverInfo.map, template: serverInfo.game});
             }
-            // render result
-            var result = `Current Modpack: ${modpack}`;
-            res.render('index', {title: 'Digby Modpack Status', currentModpack: result});
-        }
         ).catch(function (error) {
-            var result = `Server is Offline`;
-            res.render('index', {title: 'Digby Modpack Status', currentModpack: result});
+        var result = `Server is Offline`;
+        res.render('index', {title: 'Digby Modpack Status', currentModpack: result, playernum: 0, maxPlayers: 0, map: "n/a", template: "n/a"});
     })
 });
 
